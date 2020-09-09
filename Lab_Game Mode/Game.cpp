4b05f81083c9a48b4e0367c3fc56d2e6dcb02d5f
TANAPORN SRIBOONPHONG <tanapon1325@gmail.com>
2020-09-09 14:28:45 +0700

    add Lab_Game Mode

---
+++ Lab_Game Mode/Game.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+void draw_ship(int, int);
+void erase_ship(int , int );
+int main()
+{
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			  if (ch == 'a'&&x != 0)
+			  { 
+			 	draw_ship(--x, y); 
+			  }
+			  if (ch == 'd'&&x!=73) 
+			  { 
+				draw_ship(++x, y); 
+			  }
+			  fflush(stdin);
+			  if (ch == 'w'&& y != 0)
+			  {
+				 erase_ship(x, y);
+				 draw_ship(x, --y);
+			  }
+			  if (ch == 's'&&y != 20)
+			  {
+				 erase_ship(x, y);
+				 draw_ship(x, ++y);
+			  }
+				fflush(stdin);
+		}
+		Sleep(100);
+    } while (ch != 'x');
+	return 0;
+}
+void draw_ship(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	printf(" <-0-> ");
+}
+void erase_ship(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+	printf("       ");
+}
+
