8de202f8bf4f4d1546e683e85f58805c21017268
TANAPORN SRIBOONPHONG <tanapon1325@gmail.com>
2020-09-28 22:40:41 +0700

    bullet 1 shot

--- Lab_Game Mode/Game.cpp
+++ Lab_Game Mode/Game.cpp
@@ -2,41 +2,84 @@
 #include<stdio.h>
 #include<windows.h>
 #include<conio.h>
+
+void setcolor(int, int);
 void gotoxy(int, int);
 void draw_ship(int, int);
 void erase_ship(int, int);
+void draw_bullet(int, int);
+void erase_bullet(int, int);
+void setcursor(bool);
+
 int main()
 {
 	char ch = ' ';
-	int x = 38, y = 20;
+	char direct='S';
+	int shot = 0;
+	int x = 38, y = 20,x_s,y_s;
+	
+
+	setcursor(0);
 	draw_ship(x, y);
 	do {
-		if (_kbhit()) {
+		if (_kbhit())
+		{
 			ch = _getch();
-			if (ch == 'a' && x != 0)
+			if (ch == 32&&shot==0)
+		    {
+			  shot = 1;
+			  x_s = x + 3;
+			  y_s = y - 1;
+			  draw_bullet(x_s, y_s);
+		    }
+			if (ch == 'a') //L=Left
 			{
-				draw_ship(--x, y);
+				direct = 'L';
 			}
-			if (ch == 'd' && x != 73)
+			if (ch == 'd')//R=Right
 			{
-				draw_ship(++x, y);
+				direct = 'R';
 			}
-			if (ch == 'w' && y != 0)
+			if (ch == 's')//S=Stop
 			{
-				erase_ship(x, y);
-				draw_ship(x, --y);
+				direct = 'S';
 			}
-			if (ch == 's' && y != 20)
-			{
-				erase_ship(x, y);
-				draw_ship(x, ++y);
-			}
-			fflush(stdin);
 		}
+		fflush(stdin);
+
+		if (shot == 1)
+		{
+			erase_bullet(x_s, y_s);
+			if (y_s  != 0)
+			{
+				draw_bullet(x_s, --y_s);
+			}
+			else
+			{
+				shot = 0;
+			}
+		}
+		
+		if (direct == 'L' && x != 0)
+		{
+			erase_ship(x, y);
+			draw_ship(--x, y);
+		}
+		if (direct == 'R' && x != 73)
+		{
+			erase_ship(x, y);
+			draw_ship(++x, y);
+		}
+
 		Sleep(100);
+	
 	} while (ch != 'x');
+	printf("\n");
+	setcolor(0, 0);
 	return 0;
 }
+
+
 void gotoxy(int x, int y)
 {
 	COORD c = { x, y };
@@ -45,10 +88,38 @@ void gotoxy(int x, int y)
 void draw_ship(int x, int y)
 {
 	gotoxy(x, y);
+	setcolor(1, 11);
 	printf(" <-0-> ");
+	
 }
 void erase_ship(int x, int y)
 {
 	gotoxy(x, y);
+	setcolor(0, 0);
 	printf("       ");
+}
+void draw_bullet(int x, int y)
+{
+	gotoxy(x, y);
+	setcolor(11,0);
+	printf("|");
+}
+void erase_bullet(int x, int y)
+{
+	gotoxy(x, y);
+	setcolor(0, 0);
+	printf(" ");
+}
+void setcursor(bool visible)
+{
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO lpCursor;
+	lpCursor.bVisible = visible;
+	lpCursor.dwSize = 20;
+	SetConsoleCursorInfo(console, &lpCursor);
+}
+void setcolor(int fg, int bg)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, bg * 16 + fg);
 }
